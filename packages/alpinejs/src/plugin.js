166c80cd8874bf73b96a2352c55d27883895ceae
Ryan Chandler <ryangjchandler@gmail.com>
2021-06-11 00:09:25 +0100

    fix: use WeakSet to improve memory usage

--- packages/alpinejs/src/plugin.js
+++ packages/alpinejs/src/plugin.js
@@ -1,9 +1,11 @@
 import Alpine from './alpine'
 
-let plugins = new Set
+let plugins = new WeakSet
 
 export function plugin(callback, config = {}) {
     if (plugins.has(callback)) return
     
     callback(Alpine, config)
+
+    plugins.add(callback)
 }
